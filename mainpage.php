53f21e5b9189e527226da4f1a03d9cd4a2cbd7ba
NicholasPerrrin <93680381+NicholasPerrrin@users.noreply.github.com>
2024-11-03 13:12:10 -0500

    Started work on the main page.

---
+++ mainpage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta charset="utf-8">
+        <title>Food Flash</title>
+    </head>
+    <body>
+        <header>
+        </header>
+        <footer>
+        </footer>
+    </body>
+</html>
